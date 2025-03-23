0ab61df5c09a6db5528a5d7ee3d24a731e8a2658
migodlcrz <lorenzo.cap.delacruz@gmail.com>
2025-03-23 23:57:53 +0800

    Remove unused library calls

--- src/pages/DonationPage.tsx
+++ src/pages/DonationPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 const DonationPage = () => {
   const [amount, setAmount] = useState("");
--- src/pages/StorePage.tsx
+++ src/pages/StorePage.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const StorePage = () => {
   return <div>Store Page</div>;
 };
